7f8fab896f237374264d2e1d4a946f913e8091c6
grigory <max@example.org>
2026-04-01 14:07:43 +0200

    change

--- ListFiles.ps1
+++ ListFiles.ps1
@@ -3,6 +3,8 @@ Get-ChildItem -File | ForEach-Object {
     Write-Output "$($_.Name) - $($_.Length) bytes"
 }
 
+
 # here please more comments Änderung aus konflikt-branch 
 
-
+# here please more comments Änderung aus konflikt-branch 
+# fkfkpwkfpkrewpfkewrpo
