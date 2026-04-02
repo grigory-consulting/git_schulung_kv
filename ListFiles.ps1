7c6afd860be0215893412453b8f344a39e3c9ffe
grigory <max@example.org>
2026-04-02 10:25:58 +0200

    bugfix

--- ListFiles.ps1
+++ ListFiles.ps1
@@ -1,5 +1,5 @@
 # List all files in the current directory with their sizes
-et-ChildItem -File | ForEach-Object {
+Get-ChildItem -File | ForEach-Object {
 
     Write-Output "$($_.Name) - $($_.Length) Bytes"
 
